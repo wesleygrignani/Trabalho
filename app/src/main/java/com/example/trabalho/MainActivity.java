49bfe78c165d7388324e56e469366a7a49c0a380
Wesley <wesley.grignani@edu.univali.br>
2019-04-23 17:54:36 -0300

    Adicionado novas telas para compra e venda

--- app/src/main/java/com/example/trabalho/MainActivity.java
+++ app/src/main/java/com/example/trabalho/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.trabalho;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void abrirTelaVendas(View view){
+        startActivity(new Intent(getBaseContext(),tela_vender.class));
+    }
+
+    public void abrirTelaCompras(View view){
+        startActivity(new Intent(getBaseContext(),tela_comprar.class));
     }
 }
---
+++ app/src/main/java/com/example/trabalho/tela_vender.java
@@ -0,0 +1,14 @@
+package com.example.trabalho;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class tela_vender extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstaceState){
+
+        super.onCreate(savedInstaceState);
+        setContentView(R.layout.activity_vender);
+    }
+}
